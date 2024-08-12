f381c8acc5994125d8b586b26cfaaa161f3229a2
N S Vinay <110729212+NS-VINAY0024@users.noreply.github.com>
2024-08-12 02:42:39 +0000

    yo

--- 01_Basics/06_DataTypes2.js
+++ 01_Basics/06_DataTypes2.js
@@ -12,6 +12,9 @@ let userEmail;
 const id = Symbol('123') // Symbols will keep the variables unique from others. If the two variables have same value as shown below, but the symbol datatype will keep both as unique value"Paranthisus"
 const anotherId = Symbol('123')
 //console.log(id === anotherId);
+console.log(typeof id);
+
+
 
 //--------------------------------------------------------------------------------------------------
 //   REFERENCE(NON PRIMITIVE)
@@ -46,8 +49,8 @@ const myFunction = function(){
 let myaccount1 = "vinay1@gmail.com"
 let myaccount2 = myaccount1
 myaccount2 = "Vinay2@google.com"
-console.log(myaccount1);
-console.log(myaccount2);
+// console.log(myaccount1);
+// console.log(myaccount2);
 /*
     In stack memory allocation, variables are organized in a last-in, first-out manner, similar to a stack data structure. When a variable is assigned the value of another variable, a copy of the original variable is created on the stack, and this copy is then assigned to the new variable. As a result, the two variables operate independently.
 */
@@ -60,8 +63,8 @@ let vinay={
 let NSvinay=vinay
 NSvinay.email="NSvinay@google.com"
 
-console.log(vinay.email);
-console.log(NSvinay.email);
+// console.log(vinay.email);
+// console.log(NSvinay.email);
 /*
     In heap memory allocation, no copy of the first variable is created. Therefore, if we modify the object, the original value changes, and both variables will reflect the same value.
     In stack memory allocation, the variables are placed like a stack in bottom-up approach. If the first variable is assigned to second Variable as a value, then a copy of first variable is created by the stack and then assigined to the second variable. If we try to change the value of second variable, the copy of the first variable will get changed but not effects the original Variable
