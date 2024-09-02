7fda2381bb2793e4eaf31105a6ce018a99cc8bf4
N S Vinay <110729212+NS-VINAY0024@users.noreply.github.com>
2024-09-02 09:09:14 +0000

    For of

---
+++ 05_Iterations/for_of_loop.js
@@ -0,0 +1,13 @@
+// 'for of' loop
+// Synatax : for (variable of iterable) {}
+const arr = [1, 2, 3, 4, 5]
+
+for (const num of arr) {
+    //console.log(num);
+}
+
+const greetings = "Hello world!"
+for (const greet of greetings) {
+    console.log(`Each char is ${greet}`)
+}
+
