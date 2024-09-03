68662baceaf09965ac37865b060cec559494d870
N S Vinay <110729212+NS-VINAY0024@users.noreply.github.com>
2024-09-03 10:41:39 +0000

    for of loops

--- 05_Iterations/for_of_loop.js
+++ 05_Iterations/for_of_loop.js
@@ -1,5 +1,6 @@
 // 'for of' loop
 // Synatax : for (variable of iterable) {}
+//The for.of statement executes a loop that operates on a sequence of values sourced from an iterable object.
 const arr = [1, 2, 3, 4, 5]
 
 for (const num of arr) {
