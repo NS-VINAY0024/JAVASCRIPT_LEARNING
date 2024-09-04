b4cdf940a7933abe44f49fb0edad304236628a64
N S Vinay <110729212+NS-VINAY0024@users.noreply.github.com>
2024-09-04 16:51:48 +0000

    for each and Filter

---
+++ 05_Iterations/for_each_Filter.js
@@ -0,0 +1,93 @@
+// +++++++++++++++++++++++++    for each statements     +++++++++++++++
+// Def : The forEach() method of Array instances executes a provided function once for each array element
+// Example
+// array.forEach(element => { 
+    // });
+
+
+// const coding = ["js", "ruby", "java", "python", "cpp"]
+
+
+
+// coding.forEach( function (val){      // for normal function
+//     console.log(val);
+// } )
+
+// coding.forEach( (item) => {          // for Arrow function
+//     console.log(item);
+// } )
+
+// function printMe(item){              // Creating function and then using inside the for-each
+//     console.log(item);
+// }
+// coding.forEach(printMe)
+
+// coding.forEach( (item, index, arr)=> {   //  This will help to print items inside the array variable with there's index and reference of the array
+//     console.log(item, index, arr);
+// } )
+
+// Taking objects as the array's items and initializing inside the For_each
+const myCoding = [
+    {
+        languageName: "javascript",
+        languageFileName: "js"
+    },
+    {
+        languageName: "java",
+        languageFileName: "java"
+    },
+    {
+        languageName: "python",
+        languageFileName: "py"
+    },
+]
+
+myCoding.forEach( (item) => {
+    
+    console.log(item.languageName);
+} )
+
+
+
+// const values = coding.forEach( (item) => {
+//     //console.log(item);
+//     return item
+// } )
+
+// console.log(values);
+
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// const newNums = myNums.filter( (num) => {
+//     return num > 4
+// } )
+
+// const newNums = []
+
+// myNums.forEach( (num) => {
+//     if (num > 4) {
+//         newNums.push(num)
+//     }
+// } )
+
+// console.log(newNums);
+
+
+const books = [
+    { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
+    { title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008 },
+    { title: 'Book Three', genre: 'History', publish: 1999, edition: 2007 },
+    { title: 'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010 },
+    { title: 'Book Five', genre: 'Science', publish: 2009, edition: 2014 },
+    { title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010 },
+    { title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996 },
+    { title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016 },
+    { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
+  ];
+
+  let userBooks = books.filter( (bk) => bk.genre === 'History')
+
+  userBooks = books.filter( (bk) => { 
+    return bk.publish >= 1995 && bk.genre === "History"
+})
+//   console.log(userBooks);
